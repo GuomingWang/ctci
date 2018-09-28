fbc174e60ffd7e0dfbcc741085b72ab1a60f44ab
Garen <43560507+GuomingWang@users.noreply.github.com>
2018-09-28 21:23:31 +0800

    Update Question1_8.java

--- Question1_8.java
+++ Question1_8.java
@@ -1,29 +1,12 @@
 import java.util.*;
 
 public class ReverseEqual {
-    public boolean isSubstring(String father, String child) {
-        if (child.length() > father.length()) {
-            return false;
-        }
-        for (int i = 0; i < father.length(); i++) {
-            int temp = i;
-            int j = 0;
-            while(j < child.length() && child.charAt(j) == father.charAt(temp)) {
-                temp++;
-                j++;
-            }
-            if (j == child.length()) {
-                return true;
-            }
-        }
-        return false;
-    }
     public boolean checkReverseEqual(String s1, String s2) {
         // write code here
         if (s2.length() > s1.length()) {
             return false;
         }
         String s3 = s1 + s1;
-        return isSubstring(s3, s2);
+        return s3.contains(s2);
     }
 }
