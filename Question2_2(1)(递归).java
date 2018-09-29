aef295ab84b11c49f30b7c75936ba343d2a0ecf8
Garen <43560507+GuomingWang@users.noreply.github.com>
2018-09-29 13:54:10 +0800

    Update Question2_2(1)(递归).java

--- Question2_2(1)(递归).java
+++ Question2_2(1)(递归).java
@@ -10,6 +10,7 @@ public class ListNode {
 public class Solution {
     int count = 0;
     ListNode getNode = null;
+    
     public ListNode FindKthToTail(ListNode head,int k) {
         if (k <= 0 || head == null) {
             return null;
