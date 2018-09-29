a15e8b3065c52ec70fad23a6a8ece8620b5edb4d
Garen <43560507+GuomingWang@users.noreply.github.com>
2018-09-29 15:20:48 +0800

    Create Question3_5.java

---
+++ Question3_5.java
@@ -0,0 +1,35 @@
+import java.util.Stack;
+
+public class Solution {
+    Stack<Integer> stack1 = new Stack<Integer>();
+    Stack<Integer> stack2 = new Stack<Integer>();
+    
+    public void push(int node) {
+        stack1.push(node);
+    }
+    
+    public int pop() {
+        while (!stack1.empty()) {
+            Object obj = stack1.pop();
+            int value = -1;
+            if (obj instanceof Integer) {
+                value = (int)obj;
+            }
+            stack2.push(value);
+        }
+        Object getObj = stack2.pop();
+        int getValue = -1;
+        if (getObj instanceof Integer) {
+            getValue = (int)getObj;
+        }
+        while (!stack2.empty()) {
+            Object obj = stack2.pop();
+            int value = -1;
+            if (obj instanceof Integer) {
+                value = (int)obj;
+            }
+            stack1.push(value);
+        }
+        return getValue;
+    }
+}
