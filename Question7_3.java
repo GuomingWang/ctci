71ad65919078e759590d796faf5a229b53f6ba9d
Garen <43560507+GuomingWang@users.noreply.github.com>
2018-10-01 23:09:12 +0800

    Update Question7_3.java

--- Question7_3.java
+++ Question7_3.java
@@ -3,10 +3,12 @@ import java.util.*;
 public class CrossLine {
     public boolean checkCrossLine(double s1, double s2, double y1, double y2) {
         // write code here
-        if (s1 != s2) {
+        double epsilon = 1e-6;
+        // 了解浮点表示法的限制。切记不要用==检查浮点数是否相等，而是应该检查两者差值是否小于某个极小值，例如1e-6。
+        if (Math.abs(s1 - s2) > epsilon) {
             return true;
         }
-        if (y1 == y2) {
+        if (Math.abs(y1 - y2) < epsilon) {
             return true;
         }
         return false;
