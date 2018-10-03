4e0223a67dbdc332124d0641e2d8fb675698053c
Garen <43560507+GuomingWang@users.noreply.github.com>
2018-10-03 23:08:05 +0800

    Update Question7_7.java

--- Question7_7.java
+++ Question7_7.java
@@ -1,35 +1,31 @@
 import java.util.*;
 
 public class KthNumber {
-    public static int removeMin(ArrayList<Integer> arrayList) {
-        int min = arrayList.get(0);
-        for (int i = 0; i < arrayList.size(); i++) {
-            if (arrayList.get(i) < min) {
-                min = arrayList.get(i);
-            }
-        }
-        for (int i = 0; i < arrayList.size(); i++) {
-            if (arrayList.get(i) == min) {
-                arrayList.remove(i);
-                i--; // 后继元素填补空位，因此需要再检查一次
-            }
-        }
-        return min;
-    }
-
     public static int findKth(int k) {
         // write code here
-        ArrayList<Integer> arrayList = new ArrayList<>();
-        arrayList.add(3);
-        arrayList.add(5);
-        arrayList.add(7);
+        ArrayList<Integer> arrayList3 = new ArrayList<>();
+        ArrayList<Integer> arrayList5 = new ArrayList<>();
+        ArrayList<Integer> arrayList7 = new ArrayList<>();
+        arrayList3.add(3);
+        arrayList5.add(5);
+        arrayList7.add(7);
         int val = 0;
         for (int i = 0; i < k; i++) {
-            val = removeMin(arrayList);
+            val = Math.min(Math.min(arrayList3.get(0), arrayList5.get(0)), arrayList7.get(0));
             System.out.println(val);
-            arrayList.add(val * 3);
-            arrayList.add(val * 5);
-            arrayList.add(val * 7);
+            if (val == arrayList3.get(0)) {
+                arrayList3.remove(0);
+                arrayList3.add(val * 3);
+                arrayList5.add(val * 5);
+                arrayList7.add(val * 7);
+            } else if (val == arrayList5.get(0)) {
+                arrayList5.remove(0);
+                arrayList5.add(val * 5);
+                arrayList7.add(val * 7);
+            } else {
+                arrayList7.remove(0);
+                arrayList7.add(val * 7);
+            }
         }
         return val;
     }
