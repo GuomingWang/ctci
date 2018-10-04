2904e590889f8b588ed866e3e291e0dcf33ade74
Garen <43560507+GuomingWang@users.noreply.github.com>
2018-10-04 11:32:34 +0800

    Update Question9_1(非递归).java

--- Question9_1(非递归).java
+++ Question9_1(非递归).java
@@ -4,17 +4,15 @@ public class GoUpstairs {
     public int countWays(int n) {
         // write code here
         if (n < 0) {
-            return 0;
+            return 0; // 楼梯有负数阶，没有办法
         } else if (n == 0) {
-            return 1;
-        } else if (n == 1) {
-            return 1;
+            return 1; // 楼梯有0阶，只有1种方法
         } else {
             int a = 0;
-            int b = 1;
+            int b = 0;
             int c = 1;
             int sum = a + b + c;
-            for (int i = 2; i <= n; i++) {
+            for (int i = 1; i <= n; i++) {
                 sum = ((a + b) % 1000000007 + c) % 1000000007;
                 // 取模运算性质：(a+b)%c = ((a%c)+(b%c))%c
                 a = b;
